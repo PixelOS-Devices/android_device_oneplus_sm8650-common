b4ee3cd8f2b66cd518a40d132c2c5b97c3f3198b
chandu078 <chandudyavanapelli03@gmail.com>
2026-04-19 09:52:32 +0000

    sm8650-common: Reformat extract-files.py
    Change-Id: I0b06924ea149499959d10d99ab77679c7d1c1762

--- extract-files.py
+++ extract-files.py
@@ -19,19 +19,22 @@ from extract_utils.main import (
 
 namespace_imports = [
     'device/oneplus/sm8650-common',
+    'hardware/oplus',
     'hardware/qcom-caf/sm8650',
     'hardware/qcom-caf/wlan',
-    'hardware/oplus',
     'vendor/qcom/opensource/commonsys/display',
     'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/dataservices',
 ]
 
+
 def lib_fixup_odm_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'odm' else None
 
+
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
+
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
@@ -56,9 +59,9 @@ blob_fixups: blob_fixups_user_type = {
     'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff': blob_fixup()
         .add_needed('libshims_aidl_fingerprint_v3.oplus.so'),
     (
-        'odm/bin/touchDaemon',
         'odm/bin/hw/vendor-oplus-hardware-touch-V2-service',
         'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff',
+        'odm/bin/touchDaemon',
         'vendor/bin/qvrdatauploader',
     ): blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
@@ -72,7 +75,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
     'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-full.so','libprotobuf-cpp-full-21.7.so'),
+        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
     'vendor/bin/init.kernel.post_boot-memory.sh': blob_fixup()
         .regex_replace('# echo always', 'echo always'),
     'vendor/bin/system_dlkm_modprobe.sh': blob_fixup()
@@ -86,31 +89,33 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/bin/xtra-daemon',
         'vendor/lib64/libcne.so',
         'vendor/lib64/libqcc_sdk.so',
-        'vendor/lib64/libqms_client.so'
+        'vendor/lib64/libqms_client.so',
+        'vendor/lib64/vendor.libdpmframework.so',
     ): blob_fixup()
         .add_needed('libbinder_shim.so'),
-    ('vendor/etc/media_codecs_cliffs_v0.xml', 'vendor/etc/media_codecs_cliffs_v1.xml', 'vendor/etc/media_codecs_pineapple.xml'): blob_fixup()
+    (
+        'vendor/etc/media_codecs_cliffs_v0.xml',
+        'vendor/etc/media_codecs_cliffs_v1.xml',
+        'vendor/etc/media_codecs_pineapple.xml',
+    ): blob_fixup()
         .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', '')
         .regex_replace('</MediaCodecs>','    <Include href="media_codecs_dolby_audio.xml" />')
         .add_line_if_missing('</MediaCodecs>'),
-    'vendor/etc/seccomp_policy/gnss@2.0-qsap-location.policy': blob_fixup()
-        .add_line_if_missing('sched_get_priority_min: 1')
-        .add_line_if_missing('sched_get_priority_max: 1'),
-    'vendor/etc/sensors/hals.conf': blob_fixup()
-        .add_line_if_missing('sensors.oplus.so'),
-    'vendor/etc/init/vendor.dpmd.rc': blob_fixup()
-        .regex_replace(
-            r'(service\s+vendor\.dpmd\s+/vendor/bin/vendor\.dpmd\s*\n)',
-            r'\1    user root\n'
-        ),
     'vendor/etc/init/nicmd.rc': blob_fixup()
         .regex_replace(
             r'(service\s+vendor\.nicmd\s+/system/vendor/bin/nicmd\s*\n\s*class\s+main)',
             r'\1\n    user root\n    group root'
         ),
-    'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
-        .add_needed('libbinder_shim.so')
-        .add_needed('libhidlbase_shim.so'),
+    'vendor/etc/init/vendor.dpmd.rc': blob_fixup()
+        .regex_replace(
+            r'(service\s+vendor\.dpmd\s+/vendor/bin/vendor\.dpmd\s*\n)',
+            r'\1    user root\n'
+        ),
+    'vendor/etc/seccomp_policy/gnss@2.0-qsap-location.policy': blob_fixup()
+        .add_line_if_missing('sched_get_priority_min: 1')
+        .add_line_if_missing('sched_get_priority_max: 1'),
+    'vendor/etc/sensors/hals.conf': blob_fixup()
+        .add_line_if_missing('sensors.oplus.so'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('libcodec2_shim.so'),
     (
@@ -119,6 +124,8 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/liblistensoundmodel2vendor.so',
     ): blob_fixup()
         .replace_needed('libtensorflowlite_c.so', 'libtensorflowlite_c_vendor.so'),
+    'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
+        .add_needed('libhidlbase_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
